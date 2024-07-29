5b73023d6f58e803a6653f76179057a040ce12e5
Braulio Ruiz Niñoles <147655005+Braulio-Ruiz@users.noreply.github.com>
2024-07-29 15:21:25 -0400

    Update Dibujar Matrices.py

--- Dibujar Matrices.py
+++ Dibujar Matrices.py
@@ -1,23 +1,5 @@
 """
-Identificación del trabajo
-
-Módulo: 2
-Asignatura: Programacion Basica
-Docente Online: Adrian Alvarez
-Fecha de entrega: 07-09-2023
-	
-Identificación del estudiante
-
-Nombre: Braulio Ruiz Niñoles
-Carrera: Tecnico en Informatica
-
-Ejercicio 1
-
-Debes crear un programa que dibuje una matriz, según las siguientes consideraciones:
-1. Solicitud de filas
-2. Solicitud de columnas
-3. Se logre dibujar filas indicadas
-4. Se logre dibujar columnas solicitadas.
+Permite dibujar matrices.
 """
 
 # Solicitar el número de filas al usuario
